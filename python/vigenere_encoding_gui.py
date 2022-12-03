1ed3592533ced2971e801ab8ee1ae3f385b660e6
al3xkras <alexandr.krasovskiy@gmail.com>
2022-12-03 21:59:11 +0100

    TestMethodCanvas, VigenereEncodingGUI: improve appearance

--- python/vigenere_encoding_gui.py
+++ python/vigenere_encoding_gui.py
@@ -29,21 +29,27 @@ class LaTeXFrame:
         self.label = None
         self.mainframe = None
         self.canvas = None
-    def draw(self, size=(1,1)):
+
+    def draw(self, size=(1,1), included=False):
         root = self.master
 
         self.mainframe = tk.Frame(root)
-        self.mainframe.pack(side="left")
 
+        self.mainframe.pack(side="left", anchor="s")
         self.label = tk.Frame(self.mainframe)
-        self.label.pack(fill="both")
+        if included:
+            self.label.pack(side="top",anchor="w", fill="x",expand=True)
+        else:
+            self.label.pack(side="top",anchor="w")
 
         self.fig = matplotlib.figure.Figure(figsize=size, dpi=LaTeXFrame.dpi)
         self.ax = self.fig.add_subplot(111)
 
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.label)
-        self.canvas.get_tk_widget().pack(fill="both")
-        self.canvas._tkcanvas.pack(fill="both")
+
+        self.canvas.get_tk_widget().pack(fill="x")
+
+        self.canvas._tkcanvas.pack(fill="x")
 
         self.ax.get_xaxis().set_visible(False)
         self.ax.get_yaxis().set_visible(False)
@@ -65,9 +71,9 @@ class LaTeXFrame:
     def remove(self):
         self.mainframe.destroy()
 
-    def drawNextTo(self, other,size=(1,1)):
+    def drawNextTo(self, other,size=(1,1), included=False):
         self.pos=(other.pos[0] + LaTeXFrame.dpi + LaTeXFrame.offset, other.pos[1])
-        self.draw(size)
+        self.draw(size, included)
 
 class TestMethods:
     @staticmethod
@@ -148,15 +154,17 @@ class TestMethodCanvas:
         self.label=None
         self.frames=[]
         if "label" in kwargs:
-            self.label=tk.Label(self.test_frame,text=kwargs["label"], padx=2, pady=2)
+            self.label=tk.Label(self.test_frame,text=kwargs["label"], padx=2, pady=2,
+                font="Arial 11 bold")
         self.var1_label=None
         self.var1_side="top"
         if "var1" in kwargs:
             self.frames.append(tk.Frame(self.test_frame))
             fr=self.frames[len(self.frames)-1]
             self.var1_entry = tk.Entry(fr, textvariable=self.var1)
-            self.var1_label=tk.Label(fr,text=kwargs["var1"], padx=2, pady=2)
-            self.var1_label.pack(side="left")
+            self.var1_label=tk.Label(fr,text=kwargs["var1"], padx=2, pady=2,
+                font="Arial 10")
+            self.var1_label.pack(side="left",anchor="w")
             self.var1_side="left"
         else:
             self.var1_entry = tk.Entry(self.test_frame, textvariable=self.var1)
@@ -167,28 +175,30 @@ class TestMethodCanvas:
             self.frames.append(tk.Frame(self.test_frame))
             fr=self.frames[len(self.frames)-1]
             self.var2_entry = tk.Entry(fr, textvariable=self.var2)
-            self.var2_label=tk.Label(fr,text=kwargs["var2"], padx=2, pady=2)
-            self.var2_label.pack(side="left")
+            self.var2_label=tk.Label(fr,text=kwargs["var2"], padx=2, pady=2,
+                font="Arial 10")
+            self.var2_label.pack(side="left",anchor="w")
             self.var2_side="left"
         else:
             self.var2_entry = tk.Entry(self.test_frame, textvariable=self.var2)
 
         self.output_entry = tk.Entry(self.test_frame, textvariable=self.key_var,
-                                     fg="black", bg="white", bd=0, state="readonly")
+                                     fg="black", bg="white", bd=0, state="readonly",
+                                     font="Comic_Sans 11 italic")
 
         self.btn_text=tk.StringVar(self.master)
         self.ciphertext_only_btn = tk.Button(self.test_frame, command=_encodingTest, textvariable=self.btn_text)
 
     def draw(self, *args):
         if self.label is not None:
-            self.label.pack(side="top")
+            self.label.pack(side="top",pady=10,anchor="w")
         if len(args)==0:
             args=["Deduce key"]
         self.btn_text.set(args[0])
         for x in self.frames:
-            x.pack(side="top")
-        self.var1_entry.pack(side=self.var1_side, padx=5, pady=5)
-        self.var2_entry.pack(side=self.var2_side, padx=5, pady=5)
+            x.pack(side="top",anchor="w",fill="x",expand=True)
+        self.var1_entry.pack(side=self.var1_side, padx=5, pady=5, fill="x", expand=True)
+        self.var2_entry.pack(side=self.var2_side, padx=5, pady=5, fill="x", expand=True)
         self.output_entry.pack(side="top", padx=5, pady=5)
         self.ciphertext_only_btn.pack(side="top")
         self.test_frame.pack(side="left")
@@ -200,16 +210,16 @@ class VigenereEncodingGUI:
     font= "16"
     clear_delay=0.01
     post_clear_delay=1.5
-    max_frames=7
-    w=1000
-    h=450
+    max_frames=15
+    w=1225
+    h=500
     scr_size="%sx%s"%(w,h)
     def __init__(self):
         self.enc=None
         self.operating_thread=None
         self.main=tk.Tk()
         self.main.geometry(VigenereEncodingGUI.scr_size)
-        self.symbol_canvas = tk.Canvas(self.main)
+        self.symbol_canvas = tk.Canvas(self.main,background="green")
         self.anim_interrupt_ev=threading.Event()
 
         self.animation_speed=tk.DoubleVar(self.main)
@@ -236,20 +246,24 @@ class VigenereEncodingGUI:
 
         self.anim_speed_entry=tk.Entry(self.anim_fr, textvariable=self.animation_speed)
         self.anim_interrupt_btn=tk.Button(self.anim_fr, command=self._interrupt, text="Interrupt")
+
+        self.title=tk.Label(self.main,text="Vigenere encoding GUI: v1.0",
+                            font="Comic_Sans 18", pady=15)
+        self.title.pack(side="top")
         self.text_input.pack(side="top", padx=10, pady=10)
         self.key_input.pack(side="top", padx=10, pady=10)
         self.ciphertext_output.pack(side="top")
         self.button_encode.pack(side="left", padx=10, pady=10)
         self.button_decode.pack(side="left", padx=10, pady=10)
         self.btn_frame.pack(side="top")
-        tk.Label(self.anim_fr,text="Animation speed").pack(side="left")
-        self.anim_speed_entry.pack(side="left")
-        self.anim_interrupt_btn.pack(side="left")
-        self.controls_fr.pack(side="left", fill="y")
-        self.anim_fr.pack(side="top", pady=20)
+        tk.Label(self.anim_fr,text="Animation speed", font="Arial 12").pack(side="top",anchor="w")
+        self.anim_speed_entry.pack(side="left",anchor="w", padx=5, pady=5)
+        self.anim_interrupt_btn.pack(side="left",anchor="w", padx=5, pady=5)
+        self.controls_fr.pack(side="left", fill="y", pady=20)
+        self.anim_fr.pack(side="top", pady=20, anchor="w", padx=20, fill="x")
 
 
-        self.symbol_canvas.pack(side="top",anchor="w", padx=50,pady=10)
+        self.symbol_canvas.pack(side="top",anchor="w", fill="both", expand=True, padx=50,pady=10)
         self.tests_grid=tk.Frame(self.main)
 
         lock=threading.Lock()
@@ -280,15 +294,15 @@ class VigenereEncodingGUI:
         self.tests_grid.pack(side="left")
 
         self.frames=[
-            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi),
-            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi),
-            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi),
-            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi),
-            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi)
+            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi, background="white"),
+            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi, background="white"),
+            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi, background="white"),
+            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi, background="white"),
+            tk.Frame(self.symbol_canvas, width=0, height=LaTeXFrame.dpi, background="white")
         ]
         self.letter_frames_count=3
         for x in self.frames:
-            x.pack(side="top",fill="both", expand=True)
+            x.pack(side="top", fill="both", expand=True)
         self.last_symbol_encoded=[None]*len(self.frames)
         self.fragment_symbols=[]
         self.w=VigenereEncodingGUI.w
@@ -379,13 +393,13 @@ class VigenereEncodingGUI:
             sym=LaTeXFrame(self.frames[pos], (LaTeXFrame.offset, LaTeXFrame.offset + w * pos),latex)
             if _include:
                 self.fragment_symbols.append(sym)
-            sym.draw(size)
+            sym.draw(size,_include)
             self.last_symbol_encoded[pos]=sym
         else:
             sym = LaTeXFrame(self.frames[pos], (0, 0),latex)
             if _include:
                 self.fragment_symbols.append(sym)
-            sym.drawNextTo(self.last_symbol_encoded[pos],size)
+            sym.drawNextTo(self.last_symbol_encoded[pos],size,_include)
             self.last_symbol_encoded[pos]=sym
 
         if len(self.fragment_symbols) > VigenereEncodingGUI.max_frames * self.letter_frames_count:
@@ -452,7 +466,11 @@ def letter_encode_decorator(function):
         nonlocal keyLetter, encoded, textLetter
         keyLetter = kwargs["keyLetter"] if "keyLetter" in kwargs else args[1]
         textLetter = kwargs["textLetter"] if "textLetter" in kwargs else args[0]
-        encoded = function(self, *args, **kwargs)
+        encoded = "-"
+        try:
+            encoded = function(self, *args, **kwargs)
+        except:
+            pass
         gui.drawNextLetter(keyLetter, textLetter, encoded)
         lock1.release()
         return encoded
@@ -491,7 +509,10 @@ def letter_decode_decorator(function):
         nonlocal keyLetter, decoded, encodedLetter
         keyLetter = kwargs["keyLetter"] if "keyLetter" in kwargs else args[1]
         encodedLetter = kwargs["encodedLetter"] if "encodedLetter" in kwargs else args[0]
-        decoded = function(self, *args, **kwargs)
+        decoded = "-"
+        try:
+            decoded = function(self, *args, **kwargs)
+        except: pass
         gui.drawNextLetter(keyLetter, encodedLetter, decoded)
         return decoded
     return wrapper
